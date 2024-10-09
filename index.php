b71c6d3790bdf9b4a909c32d9eefeccb04975f72
Jules VINET LATRILLE <julesvinet64@gmail.com>
2024-10-09 16:57:01 +0200

    ON A TROUVE UN THEME !!!

--- index.php
+++ index.php
@@ -5,13 +5,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Liste de Produits</title>
+    <title>Site peluches</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <h1>Gros titre pour le projet</h1>
-    <p>Texte</p>
+
+    <header>
+        <nav class="menu">
+            <div class="menu-left">
+                <a href="index.php">Page d'accueil</a>
+            </div>
+            <div class="menu-right">
+                <button class="btn-connect">Se connecter</button>
+            </div>
+        </nav>
+    </header>
+
+
+    <h1>Vente de peluches</h1>
+    <p>Plein plein de pitites pilouches à vendre</p>
 
     <?php
     // Lie le fichier .json
@@ -26,7 +39,7 @@
         echo '<div class="produit">';
         echo '<img src="' . htmlspecialchars($product['image']) . '" alt="' . htmlspecialchars($product['nomProd']) . '">';
         echo '<h2>' . htmlspecialchars($product['nomProd']) . '</h2>';
-        echo '<p class="prixProd">' . $product['prixProd']. ' €</p>';
+        echo '<p class="prixProd">' . $product['prixProd'] . ' €</p>';
         echo '<p>' . htmlspecialchars($product['description']) . '</p>';
         echo '<button>Ajouter au panier</button>';
         echo '</div>';
